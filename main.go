3255a5f6b5ee3de1fb098bba2d74e9781e32d611
abidta001 <abidta001@gmail.com>
2025-01-21 18:40:29 +0530

    cors initialized

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/joho/godotenv"
 
 	"horizon/config"
@@ -16,6 +19,21 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Handle preflight request
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Example response
+		fmt.Fprintf(w, "CORS enabled for all origins!")
+
+	})
 
 	config.InitDB()
 
